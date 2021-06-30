15cf8086af57acba7a355e9975f4fafa19795776
Krzyzu84 <krzysztofptak97@gmail.com>
2021-06-30 20:57:14 +0200

    added Login and Register functionality with validation

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -10,36 +10,101 @@ import {
 } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import firebase from '../../firebase';
+import md5 from 'md5';
 
 class Register extends React.Component {
   state = {
       username: '',
       email: '',
       password: '',
-      passwordConfirmation: ''
+      passwordConfirmation: '',
+      errors: [],
+      loading: false,
+      usersRef: firebase.database().ref('users')
   };
 
   handleChange = (event) => {
       this.setState({[event.target.name]: event.target.value});
   };
 
+  isFormValid = () => {
+      let errors= [];
+      let error;
+    if(this.isFormEmpty(this.state)) {
+        error = {message: 'Fill in all fields'};
+        this.setState({errors: errors.concat(error)})
+        return false;
+    } else if (!this.isPasswordValid(this.state)) {
+        let error = {message: 'Password is not valid'}
+        this.setState({errors: errors.concat(error)});
+        return false;
+    } else {
+        return true;
+    }
+  }
+
+  isFormEmpty = ({username, email, password, passwordConfirmation}) => {
+    return !username.length || !email.length || !password.length || !passwordConfirmation.length;
+  }
+
+  isPasswordValid = ({password, passwordConfirmation}) => {
+    if(password.length < 6 || passwordConfirmation.length < 6) {
+        return false;
+    } else if(password !== passwordConfirmation) {
+        return false;
+    } else {
+        return true;
+    }
+  }
+
+  displayErros = (erros) =>  erros.map((error,i) => <p key={i}>{error.message}</p>);
+
+  saveUser = (createdUser) => {
+    return this.state.usersRef.child(createdUser.user.uid).set({
+        name: createdUser.user.displayName,
+        avatar: createdUser.user.photoURL
+    });
+  }
+
   handleSubmit = (event) => {
-      event.preventDefault();
-      firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(createdUser => {
-          console.log(createdUser);
-      }).catch(err =>{ console.log(err)});
+    event.preventDefault();
+
+    if(this.isFormValid()){
+        this.setState({errors: [], loading: true});
+        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(createdUser => {
+            console.log(createdUser);
+            createdUser.user.updateProfile({
+                displayName: this.state.username,
+                photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`
+            }).then(() => {
+                this.saveUser(createdUser).then(() => {
+                    console.log("user saved");
+                    this.setState({loading: false});
+                })
+            }).catch(err =>{  
+                this.setState({errors: this.state.errors.concat(err),loading: false});
+                console.log(err);
+            });
+            
+        }).catch(err =>{  
+            this.setState({errors: this.state.errors.concat(err),loading: false});
+            console.log(err);
+        });
+    }
 
   }
 
+
+
   render() {
 
-    const {username, email, password, passwordConfirmation} = this.state;
+    const {username, email, password, passwordConfirmation, errors, loading} = this.state;
 
     return (
       <Grid style={{ marginTop: 100 }} textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" icon color="orange" textAlign="center">
+          <Header as="h1" icon color="orange" textAlign="center">
             <Icon name="puzzle piece" color="orange" />
             Register for DevChat
           </Header>
@@ -89,11 +154,17 @@ class Register extends React.Component {
                 value={passwordConfirmation}
               />
 
-              <Button color="orange" fluid size="large">
+              <Button disabled={loading} className={loading? 'loading': ''} color="orange" fluid size="large">
                 Submit
               </Button>
             </Segment>
           </Form>
+          {errors.length > 0 && (
+              <Message error>
+                  <h3>Error</h3>
+                  {this.displayErros(errors)}
+              </Message>
+          )}
           <Message>
             Already a user? <Link to="/login">Login</Link>
           </Message>
